d12bbbb1dcfca837ed2fc3e8a5fc0642235247e2
Vamsi Krishna GUNTURU <45221212+vamsikrishna-g@users.noreply.github.com>
2023-09-07 22:47:11 +0530

    Update publication.js

--- publication.js
+++ publication.js
@@ -5,6 +5,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Prediction of evapotranspiration using a nonlinear local approximation approach",
                 date: "May 2022",
+                confname:"EGU",
                 type: "conference",
             },
             {
@@ -12,6 +13,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Prediction of Evapotranspiration Using Complex Networks",
                 date: "August 2022",
+                confname:"AOGS",
                 type: "conference",
             },
             {
@@ -19,6 +21,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Behavioural study of Evapotranspiration time series: Visibility Graph Analysis",
                 date: "December 2022",
+                confname:"AGU",
                 type: "conference",
             },
             {
@@ -26,6 +29,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Monthly evapotranspiration trends across India using the Hurst exponent",
                 date: "December 2022",
+                confname:"HYDRO",
                 type: "conference",
             },
             {
@@ -33,6 +37,7 @@ const publications = [
                 author2:"Krishna, G.V.",
                 title: "Comprehending Architecture and Varieties of Temple Tanks in India",
                 date: "February 2023",
+                confname:"IKS",
                 type: "conference",
             },
             {
@@ -40,6 +45,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Actual Evapotranspiration in India: Trend, Persistence, and Homogeneity",
                 date: "In review",
+                confname:"",
                 type: "journal",
             },
             {
@@ -47,6 +53,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Analysis of Evapotranspiration using Complex Networks and Chaos Theory",
                 date: "August 2023",
+                confname:"AOGS",
                 type: "conference",
             },
             {
@@ -54,6 +61,7 @@ const publications = [
                 author2:"Jothiprakash, V., Sivakumar, B.",
                 title: "Dynamic Behaviour of Daily Evapotranspiration Time Series: Use of Visibility Graphs",
                 date: "In review",
+                confname:"",
                 type: "journal",
             },
     // More publication data entries...
@@ -76,14 +84,13 @@ function filterByType(type) {
         const author2 = pub.author2 === "Krishna, G.V." ? `<strong>${pub.author2}</strong>` : pub.author2;
 
 
-        listItem.innerHTML = `${author1} ${author2}, ${pub.title}, <i>${pub.date}</i>`;
+        listItem.innerHTML = `${author1} ${author2}, ${pub.title}, ${pub.confname}, <i>${pub.date}</i>`;
         publicationList.appendChild(listItem);
     });
 }
 
 // Initial display (show all publications)
 filterByType("all");
-
 
 
 
